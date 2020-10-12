8221d8b70b8bd2e0930cad139accab6dae7896dc
Jeremy Rodrigues <jrodrigues@ripple.com>
2020-10-11 17:51:33 -0700

    Started basic logging.

--- base_bot/base_bot/base_bot.py
+++ base_bot/base_bot/base_bot.py
@@ -1,12 +1,23 @@
 from sys import platform
+import logging
 
 from core.raspbian_os_type import RaspianBaseBot
 from core.macos_os_type import MacosBaseBot
 
+logging.basicConfig(
+    filename="trash_problem.log",
+    filemode='w',
+    format='%(name)s - %(levelname)s - %(message)s',
+    level=logging.DEBUG
+)
+
+
 if platform == "linux" or platform == "linux2":
+    logging.debug("linux")
     RaspianBaseBot().assistant()
 
 elif platform == "darwin":
+    logging.debug("MacOS")
     MacosBaseBot().assistant()
 
 else:
