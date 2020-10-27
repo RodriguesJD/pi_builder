72a712abe10ddb5610fde1fd410bff6562a677be
Jeremy Rodrigues <jeremyrodrigues@tank.local>
2020-10-27 12:28:39 -0700

    Wait for disk to mount before moving to next step.

--- base_bot/base_bot/core/macos_os_type.py
+++ base_bot/base_bot/core/macos_os_type.py
@@ -23,10 +23,15 @@ logger.addHandler(file_handler)
 logger.addHandler(stream_handler)
 
 
+def sub_proc(command):
+    subprocess_call = subprocess.Popen([command], shell=True, stdout=subprocess.PIPE).communicate()
+    return subprocess_call[0].decode()
+
+
 class MacosBaseBot:
-    """Installs raspbain on an sd card
+    """Installs ubuntu on an sd card
     """
-    def __init__(self, sd_card_name="RASPBIAN"):
+    def __init__(self, sd_card_name="system-boot"):
         self.sd_card_name = sd_card_name
 
         self.disk_number = False
@@ -42,18 +47,15 @@ class MacosBaseBot:
         logger.debug("MacosBaseBot.gather_disk_number")
 
         # create subprocess to run terminal command
-        diskutil_list = subprocess.Popen("diskutil list", shell=True, stdout=subprocess.PIPE)
-        outp = diskutil_list.stdout
+        diskutil_list = sub_proc("diskutil list").split("\n")
         external_disks = []
 
-        for line in outp:
+        for line in diskutil_list:
             external_disk = "(external, physical)"
 
             # Convert byte string to ascii <- I think its ascii
-            decoded_line = line.decode()
-
-            if external_disk in decoded_line:
-                disk_num = decoded_line.split(external_disk)[0].split("disk")[1]
+            if external_disk in line:
+                disk_num = line.split(external_disk)[0].split("disk")[1]
                 if int(disk_num):
                     external_disks.append(disk_num.replace(" ", ""))
 
@@ -64,24 +66,26 @@ class MacosBaseBot:
             raise Exception("There is more than 1 external disk. Fix this issue.")
         else:
             self.disk_number = external_disks[0]
+            logger.info(f"Disk Number is {external_disks[0]}")
             return external_disks[0]
 
     def gather_disk_path(self):
         logger.debug("MacosBaseBot.gather_disk_path")
         path_to_disk = f"/dev/disk{self.disk_number}"
+        logger.info(f"Path to disk {path_to_disk}")
         self.disk_path = path_to_disk
 
         return path_to_disk
 
     def erase_disk(self):
         logger.debug("MacosBaseBot.erase_disk")
-        subprocess.Popen([f"diskutil eraseDisk ExFAT {self.sd_card_name} MBRFormat {self.disk_path}"],
-                         shell=True, stdout=subprocess.PIPE).communicate()
+        command = f"diskutil eraseDisk ExFAT {self.sd_card_name} MBRFormat {self.disk_path}"
+        sub_proc(command)
 
     def unmount_disk(self):
         logger.debug("MacosBaseBot.unmount_disk")
-        subprocess.Popen([f"diskutil unmountDisk {self.disk_path}"],
-                         shell=True, stdout=subprocess.PIPE).communicate()
+        command = f"diskutil unmountDisk {self.disk_path}"
+        sub_proc(command)
 
     def install_os(self):
         logger.debug("MacosBaseBot.install_os")
@@ -95,21 +99,33 @@ class MacosBaseBot:
                             "This usually means you're not in the same directory as the python file.")
 
         path_to_image = Path(os.getcwd(), pi_img)
-
-        subprocess.Popen([f"sudo dd bs=1m if={path_to_image} of={self.disk_path} conv=sync"],
-                         shell=True, stdout=subprocess.PIPE).communicate()
+        command = f"sudo dd bs=1m if={path_to_image} of={self.disk_path} conv=sync"
+        command_output = sub_proc(command)
+        logger.debug(f"MacosBaseBot.install_os time to write to sd card {command_output}.")
 
     def mount_disk(self):
         logger.debug("MacosBaseBot.mount_disk")
-        subprocess.Popen([f"diskutil mountDisk {self.disk_path}"],
-                         shell=True, stdout=subprocess.PIPE).communicate()
+        try_command = True
+        attempts = 0
+        max_attempts = 3
+        while try_command:
+            command = f"diskutil mountDisk {self.disk_path}"
+            command_output = sub_proc(command)
+            if "Volume(s) mounted successfully" in command_output:
+                try_command = False
+            else:
+                attempts += 1
+                if attempts == 3:
+                    raise Exception("3 attempts failed to mount disk")
+
 
     def enable_ssh(self):
         # TODO the ssh_path is wrong
         logger.debug("MacosBaseBot.enable_ssh")
         ssh_path = Path("/Volumes/system-boot/ssh")
         if not os.path.isfile(ssh_path):
-            subprocess.Popen(["touch {}".format(ssh_path)], shell=True, stdout=subprocess.PIPE).communicate()
+            command = f"touch {ssh_path}"
+            sub_proc(command)
 
     @staticmethod
     def configure_wifi():
@@ -118,8 +134,8 @@ class MacosBaseBot:
         # TODO network config path is wrong
         network_config_path = Path("/Volumes/system-boot/network-config")
         if not os.path.isfile(network_config_path):
-            subprocess.Popen([f"touch {network_config_path}".format(network_config_path)], shell=True, stdout=subprocess.PIPE).communicate()
-
+            command =f"touch {network_config_path}"
+            sub_proc(command)
         ssid = input("What is the ssid\n")
         wifi_password = input("What is the wifi password\n")
         wifi_dict = {
@@ -149,7 +165,6 @@ class MacosBaseBot:
         logger.debug("MacosBaseBot.assistant")
 
         if not self.gather_disk_number():
-            # TODO change this to wait for the sd card to be mounted.
             raise Exception("Cant find the sd card's mounted disk number")
 
         # Generate the disk path
